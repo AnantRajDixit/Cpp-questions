e4a1463f6f1634698b2cb2d938a3124d424c0023
Anant Raj Dixit <ardixit1234@gmail.com>
2026-03-15 17:55:49 +0530

    km_to_metre.cpp in Basics

---
+++ Basics/km_to_metre.cpp
@@ -0,0 +1,13 @@
+//Convert kilometers to meters.
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a;//double liya instead of float
+    cout<<"Enter the kilometres: ";
+    cin>>a;// could have taken kilometre instd. of a
+    float metre = a*1000;
+    cout<< "The number of metres is "<<metre<<endl;
+    return 0;
+}
+
