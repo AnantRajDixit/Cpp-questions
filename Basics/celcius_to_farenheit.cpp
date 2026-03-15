32fd8978c14281f30ca85679061685d6349b04c1
Anant Raj Dixit <ardixit1234@gmail.com>
2026-03-15 17:36:42 +0530

    Added celcius_to_farenheit.cpp in Basics

---
+++ Basics/celcius_to_farenheit.cpp
@@ -0,0 +1,15 @@
+//Convert Celsius to Fahrenheit.
+#include<iostream>
+using namespace std;
+
+int main(){
+    float C;
+    cout<<"enter the value to be converted"<<endl;
+    cin>>C;
+
+    cout<< "when converted in farenheit the value is "<<C*9/5 +32<<endl;
+    
+    return 0;
+
+
+    }
