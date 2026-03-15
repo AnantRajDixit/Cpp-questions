00c74b56c1ebdc6c39251489febf39c32c912f10
Anant Raj Dixit <ardixit1234@gmail.com>
2026-03-15 19:40:44 +0530

    area_of_a_circle.cpp in Basics

---
+++ Basics/area_of_a_circle.cpp
@@ -0,0 +1,13 @@
+//Find area of a circle.
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter the radius: ";
+    cin>>r;
+
+    float area = 3.14*r*r;
+    cout<<"The area of the circle is"<<area<<endl;
+
+    return 0;
+}
