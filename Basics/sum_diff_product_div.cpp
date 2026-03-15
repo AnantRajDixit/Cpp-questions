d3d71b4ad2530e502f24267fd502cc7d54d102a2
Anant Raj Dixit <ardixit1234@gmail.com>
2026-03-15 17:22:25 +0530

    Added sum_diff_product_div.cpp in Basics

---
+++ Basics/sum_diff_product_div.cpp
@@ -0,0 +1,19 @@
+//Take two numbers and print sum, difference, product, division.
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Enter the first number"<<endl;
+    cin>>a;
+    cout<<"Enter the second number"<<endl;
+    cin>>b;
+
+    cout<<"sum of two number is : "<<a+b<<endl;
+    cout<<"Differnce between the two number is: "<<a-b<<endl;
+    cout<<"Product of the two number is: "<<a*b<<endl;
+    cout<<"Division of the two number is: "<<a/b<<endl;
+    
+    return 0;
+    
+}
