0af348793e06716493a55b42ae8df9eb407b63fc
Anant Raj Dixit <ardixit1234@gmail.com>
2026-03-15 17:12:34 +0530

    Added add_two_numbers.cpp in Basics

---
+++ Basics/add_two_numbers.cpp
@@ -0,0 +1,14 @@
+//Take two numbers and print their sum.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout<<"Enter the first value"<<endl;
+    cin>>a;
+    cout<<"Enter the second value"<<endl;
+    cin>>b;
+    cout<<"The sum is: "<<a+b<<endl;
+    return 0;
+}
