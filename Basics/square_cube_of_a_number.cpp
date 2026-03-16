3e8e99451d01f3d30c8239bb9e3cff820b8e2572
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-16 13:11:10 +0530

    Create square_cube_of_a_number.cpp

---
+++ Basics/square_cube_of_a_number.cpp
@@ -0,0 +1,18 @@
+//Find square and cube of a number.
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a;
+
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    float square = a*a;
+    float cube = a*a*a;
+
+    cout<<"The square is: "<<square<<endl;
+    cout<<"The cube is: "<<cube<<endl;
+
+    return 0;
+}
