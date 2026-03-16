c38a8d213befae5df50fb22d6243fdcc4d705899
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-16 13:58:11 +0530

    Update days_to_weeks_years_months.cpp

--- Basics/days_to_weeks_years_months.cpp
+++ Basics/days_to_weeks_years_months.cpp
@@ -12,6 +12,10 @@ int main(){
     int weeks = days/7;
     int months = days/30;
     int years = days/365;
+    // Can be done like this too for accuracy
+    int years = days / 365;                 // full years
+    int weeks = (days % 365) / 7;           // weeks from remaining days
+    int remainingDays = (days % 365) % 7;   // leftover days
 
     cout<<"The number of weeks are: "<<weeks<<endl;
     cout<<"The number of months are: "<<months<<endl;
