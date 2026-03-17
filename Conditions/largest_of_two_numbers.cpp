bca15385f40671a0e9be626f298e7b3fb14d8613
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-17 22:38:56 +0530

    Create largest_of_two_numbers.cpp

---
+++ Conditions/largest_of_two_numbers.cpp
@@ -0,0 +1,21 @@
+//Find largest of two numbers.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b ;
+    cout<<"Enter first number: ";
+    cin>>a;
+    
+    cout<<"Enter the second number: ";
+    cin>>b;
+    if (a>b)
+    {
+        cout<<"first number is greater than second";
+    }
+    else
+    {
+        cout<<"Second number is greater";
+    }   
+    return 0;
+}
