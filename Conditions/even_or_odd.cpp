42716abfbbeb3f1de6b9276967c1a7d678a8f7f0
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-17 20:55:42 +0530

    Create even_or_odd.cpp

---
+++ Conditions/even_or_odd.cpp
@@ -0,0 +1,21 @@
+//Check if number is even or odd.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<"Even";
+
+    }
+    else{
+        cout<<"Odd";
+
+    }
+
+    return 0;
+}
