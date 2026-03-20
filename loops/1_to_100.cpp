18b0425559d3af137902505c46d855d380a8e18c
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-20 16:58:33 +0530

    Create 1_to_100.cpp

---
+++ loops/1_to_100.cpp
@@ -0,0 +1,14 @@
+//Print numbers 1 to 100.
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    for (int i = 1; i <=100; i++)
+    {
+       cout<<"The numbers are: "<<i<<endl;
+    }
+    return 0;
+    
+}
