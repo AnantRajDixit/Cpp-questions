3c679266f82e74713e47b265b6e61861e128e564
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-22 13:46:34 +0530

    Create factorial_of_a_number.cpp

---
+++ loops/factorial_of_a_number.cpp
@@ -0,0 +1,17 @@
+//Find factorial of a number.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, fact = 1;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    fact *= i;
+    {
+        cout<<fact;
+    }
+    return 0;
+}
