201f36b8b3edaf550f97c06bfbed0285569a1cb3
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-22 22:50:10 +0530

    Create reverse_of_a_number.cpp

---
+++ loops/reverse_of_a_number.cpp
@@ -0,0 +1,17 @@
+//Reverse a number.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for (int i = n; i >= 1; i--)
+    {
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
