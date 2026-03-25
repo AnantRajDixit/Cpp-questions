dc02f58826568fa5a5b72ccb35861feca10ff83d
AnantRajDixit <146917208+AnantRajDixit@users.noreply.github.com>
2026-03-25 22:33:56 +0530

    Create sum_of_arrays.cpp

---
+++ array/sum_of_arrays.cpp
@@ -0,0 +1,14 @@
+//Find sum of array elements.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[4] = {1,2,3,4};
+
+    cout<<"The sum of arrays are: "<< arr[0] + arr[1]+ arr[2]+ arr[3]<< endl;
+
+    return 0;
+
+}
